98c7547efb24c0c52f000e6c3bd235e5ba724e20
Tom Powell <tom@powell.io>
2019-06-16 15:51:43 -0700

    Create github projects, if needed

--- lib.py
+++ lib.py
@@ -8,29 +8,34 @@ class Config:
     GERRIT_USER = os.environ.get("GERRIT_USER")
     GERRIT_PASS = os.environ.get("GERRIT_PASS")
 
-def get_projects(auth=None):
-    url = "https://review.lineageos.org/a/projects/?t" if auth else "https://review.lineageos.org/projects/?t"
-    resp = requests.get(url, auth=auth)
-    if resp.status_code != 200:
-        raise Exception(f"Error communicating with gerrit: {resp.text}")
-    projects = json.loads(resp.text[5:])
-    nodes = {}
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
-    for name, project in projects.items():
-        nodes[name] = []
+class Gerrit:
+    @staticmethod
+    def get_projects(auth=None):
+        url = "https://review.lineageos.org/a/projects/?t" if auth else "https://review.lineageos.org/projects/?t"
+        resp = requests.get(url, auth=auth)
+        if resp.status_code != 200:
+            raise Exception(f"Error communicating with gerrit: {resp.text}")
+        projects = json.loads(resp.text[5:])
+        nodes = {}
 
-    for name, project in projects.items():
-        parent = project.get("parent")
-        if parent:
-            nodes[parent].append(name)
-    for project in nodes.keys():
-        nodes[project] = sorted(nodes[project])
-    return nodes
+        for name, project in projects.items():
+            nodes[name] = []
 
-def update_parent(child, parent, auth=None):
-    child = quote_plus(child)
-    url = f"https://review.lineageos.org/a/projects/{child}/parent" if auth else f"https://review.lineageos.org/projects/{child}/parent"
-    print(f"Updating {child}'s parent to {parent}")
-    resp = requests.put(url, json=({"parent": parent, "commit_message": "Auto update from gerrit_config"}), auth=auth)
-    if resp.status_code != 200:
-        raise Exception(f"Error communicating with gerrit: {resp.text}")
+        for name, project in projects.items():
+            parent = project.get("parent")
+            if parent:
+                nodes[parent].append(name)
+        for project in nodes.keys():
+            nodes[project] = sorted(nodes[project])
+        return nodes
+
+    @staticmethod
+    def update_parent(child, parent, auth=None):
+        child = quote_plus(child)
+        url = f"https://review.lineageos.org/a/projects/{child}/parent" if auth else f"https://review.lineageos.org/projects/{child}/parent"
+        print(f"Updating {child}'s parent to {parent}")
+        resp = requests.put(url, json=({"parent": parent, "commit_message": "Auto update from gerrit_config"}), auth=auth)
+        if resp.status_code != 200:
+            raise Exception(f"Error communicating with gerrit: {resp.text}")
--- update.py
+++ update.py
@@ -1,17 +1,22 @@
 import yaml
 import requests
 
-from lib import get_projects, update_parent, Config
+from github import Github
+
+from lib import Gerrit, Config
+
 
 if Config.GERRIT_USER and Config.GERRIT_PASS:
     auth = requests.auth.HTTPBasicAuth(Config.GERRIT_USER, Config.GERRIT_PASS)
 else:
     auth = None
 
+print("Updating gerrit permissions...")
+
 with open("structure.yml", "r") as f:
     desired_projects = yaml.load(f.read())
 
-live_projects = get_projects(auth)
+live_projects = Gerrit.get_projects(auth)
 
 changes = {}
 
@@ -29,6 +34,26 @@ for parent, children in desired_projects.items():
 if changes:
     for parent, children in changes.items():
         for child in children:
-            update_parent(child, parent, auth)
+            Gerrit.update_parent(child, parent, auth)
+
+print("Creating github repos...")
+
+g = Github(Config.GITHUB_TOKEN)
+
+github_projects = {x.full_name for x in g.get_organization("LineageOS").get_repos()}
+gerrit_projects = set()
+for parent, children in live_projects.items():
+    if parent.startswith("LineageOS/"):
+        gerrit_projects.add(parent)
+    for child in children:
+        if child.startswith("LineageOS/"):
+            gerrit_projects.add(child)
+
+missing = gerrit_projects - github_projects
+
+for repo in missing:
+    print(f"Creating {repo} on github...")
+    g.get_organization("LineageOS").create_repo(repo.replace("LineageOS/",""), has_wiki=False, has_downloads=False, has_projects=False, has_issues=False, private=False)
+
 
 print("Done!")
