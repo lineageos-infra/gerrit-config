7ac88af015577efbf515aa7a3b15762241e9d358
LuK1337 <priv.luk@gmail.com>
2026-09-23 22:49:45 +0200

    Set GHA enabled repos via update.py

--- update.py
+++ update.py
@@ -13,6 +13,9 @@ github = Github(Config.GITHUB_TOKEN)
 with open("structure.yml", "r") as f:
     wanted = yaml.load(f.read(), Loader=yaml.BaseLoader)
 
+with open("gha_enabled_repos.yml", "r") as f:
+    gha_enabled_repos = yaml.load(f.read(), Loader=yaml.BaseLoader)
+
 live = gerrit.get_projects()
 
 
@@ -38,21 +41,22 @@ live = gerrit.get_projects()
 print("Creating github repos...")
 
 
-github_projects = {x.full_name for x in github.get_organization("LineageOS").get_repos()}
+github_org = github.get_organization("LineageOS")
+github_projects = {x.id: x.full_name for x in github_org.get_repos()}
 gerrit_projects = set()
 
 for parent, children in wanted.items():
     gerrit_projects.add(parent)
     [gerrit_projects.add(x) for x in children]
 
-missing = gerrit_projects - github_projects
+missing = gerrit_projects - set(github_projects.values())
 
 for repo in missing:
     if not repo.startswith("LineageOS/"):
         continue
     print(f"Creating {repo} on github...")
-    github.get_organization("LineageOS").create_repo(repo.replace("LineageOS/",""), has_wiki=False, has_downloads=False, has_projects=False, has_issues=False, private=False)
-    github.get_organization("LineageOS").get_repo(repo.replace("LineageOS/","")).edit(has_pull_requests=False)
+    github_org.create_repo(repo.replace("LineageOS/",""), has_wiki=False, has_downloads=False, has_projects=False, has_issues=False, private=False)
+    github_org.get_repo(repo.replace("LineageOS/","")).edit(has_pull_requests=False)
 
 print("Updating gerrit permissions...")
 
@@ -67,5 +71,11 @@ for group in [x for x in wanted.keys() if x.startswith("PROJECT-") or x.startswi
     if group not in gerrit_groups:
         print(f"adding {group}")
         gerrit.create_group(group)
+
+print("Updating GHA allowed repos...")
+
+gha_enabled_repos_ids = [k for k, v in github_projects.items() if v in gha_enabled_repos]
+github._requester.requestJsonAndCheck("PUT", f"{github_org.url}/actions/permissions/repositories",
+                                      input={"selected_repository_ids": gha_enabled_repos_ids})
 
 print("Done!")
